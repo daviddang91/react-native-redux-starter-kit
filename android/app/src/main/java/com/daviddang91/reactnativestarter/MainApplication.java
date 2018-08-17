410dbf27b694dad6ef6ee3a023d7e306d3af3499
David Dang <david.dang@Davids-MacBook-Pro.local>
2018-08-17 14:40:31 +0700

    Remove package

--- android/app/src/main/java/com/daviddang91/reactnativestarter/MainApplication.java
+++ android/app/src/main/java/com/daviddang91/reactnativestarter/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new SplashScreenReactPackage(),
-            new RNCameraPackage(),
             new VectorIconsPackage()
       );
     }
